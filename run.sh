48549366b1029af2267c9e5bdd3ccbf6a83e5d25
Matthew Kornfield <mckornfield@gmail.com>
2018-04-16 01:28:15 +0000

    Update plugins and run to mount ssh folder

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ IMAGE=jumpstart/jenkins:latest
 
 JENKINS_HOME=$DIR/jenkins_home
 JENKINS_SECRETS=$DIR/secrets
+SSH_DIR=$DIR/ssh
 
 docker run -p 8080:8080 -p 50000:50000 \
   -v $JENKINS_HOME:/Users/jenkins_home \
   -v $JENKINS_SECRETS:/secrets \
+  -v $SSH_DIR:/usr/share/jenkins/ref/.ssh \
   --name jenkins -d \
   $IMAGE
